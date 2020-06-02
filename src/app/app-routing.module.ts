e2146c0c320539dc6d4d2d4ac1748377aa619bc2
cyproto <kyashkarande@gmail.com>
2020-06-02 21:02:12 +0100

    app: Changes: - Remove boilerplate stuff. - Add routes to all modules.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './shared/guard/auth.guard';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    loadChildren: './login/login.module#LoginModule'
+  },
+  {
+    path: 'signup',
+    loadChildren: './signup/signup.module#SignupModule'
+  },
+  {
+    path: 'todo-app',
+    loadChildren: './todo-app/todo-app.module#TodoAppModule',
+    //canActivate: [AuthGuard]
+  },
+  { 
+    path: '**', 
+    redirectTo: 'login' 
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { LayoutModule } from '@angular/cdk/layout';
+import { OverlayModule } from '@angular/cdk/overlay';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthGuard } from './shared/guard/auth.guard';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+      BrowserModule,
+      AppRoutingModule,
+      BrowserAnimationsModule,
+      LayoutModule,
+      OverlayModule,
+      HttpClientModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ AuthGuard ],
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
+
